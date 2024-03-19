14ae13c979464c51ebe8ca4a1ae02e9aa4949c37
Yehor Tertyshnyk <egor.tertyshnyk@gmail.com>
2024-03-19 01:23:00 +0100

    Now its function

--- NtCounter2.py
+++ NtCounter2.py
@@ -1,28 +1,31 @@
 from collections import Counter
 
-threshold = 0.7
+def NtCounter(sequences, threshold):
 
+    for sn in range(len(sequences[0])):
+        column = ''.join(seq[sn] for seq in sequences) 
+        c = Counter(column)
+        r = {}
+        most_freq_seq = []
+        print(column)
+        for key in c:
+            r[key] = c[key]/len(column)
+        for nucleotide, frequency in r.items():
+            if frequency >= threshold:
+                most_freq_seq.append(nucleotide)
+            else: 
+                print("All below threshold!")
+        print(r)
+        print(most_freq_seq) 
+
+# for test
+threshold = 0.7
 seq1 = "ATCATCATCATCGTT"
 seq2 = "TCATCATCATCGTTT"
 seq3 = "CATCATCATCGTTGA"
 seq4 = "ATCATCATCGTTGAT"
-
 sequences = [seq1,seq2,seq3,seq4]
 
-for sn in range(len(sequences[0])):
-    column = seq1[sn] + seq2[sn] + seq3[sn] + seq4[sn]
-    c = Counter(column)
-    r = {}
-    most_freq_seq = []
-    print(column)
-    for key in c:
-        r[key] = c[key]/len(column)
-    for nucleotide, frequency in r.items():
-        if frequency >= threshold:
-            most_freq_seq.append(nucleotide)
-        else: 
-            print("All below threshold!")
-    print(r)
-    print(most_freq_seq)
-    #print(2/len(column)) 
+# Call function 
 
+NtCounter(sequences, threshold)
